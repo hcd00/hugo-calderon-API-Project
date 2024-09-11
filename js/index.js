67a68a2afaaf8b805ab735d68b7b6c813e184420
Hugo Delgado <hugo.cdelgado925@gmail.com>
2024-09-11 05:39:43 -0700

    added html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const section = document.getElementById('first');
+const text = document.getElementById('text');
 const apiKey = '63f990452fcc30086d6de43fccaab951';
 const githubUrl = 'https://api.github.com/users/hcd00/repos';
 fetch(githubUrl)
@@ -22,12 +24,35 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch("https://v3.football.api-sports.io/teams?id=33", requestOptions)
-  .then(response => response.json())
-  .then(result => {
-    const team = result.response[0].team.name;
-    const venue = result.response[0].venue.name;
-    console.log("Team:", team);
-    console.log("venue", venue);
-  })
-  .catch(error => console.log('error', error));
+const getTeam = async () => {
+  try {
+    const response = await fetch("https://v3.football.api-sports.io/teams?id=33", requestOptions)
+    const result = await response.json();
+    return result.response;
+  } catch (e) {
+    console.log('Error.', e);
+  }
+};
+
+getTeam().then(data => {
+  console.log(data);
+  text.innerHTML = data[0].team.name + '' + data[0].venue.name;
+
+})
+
+const getStandings = async () => {
+  try {
+    const response = await fetch("https://v3.football.api-sports.io/standings?league=39&season=2020", requestOptions)
+    const result = await response.json();
+    console.log(result);
+    console.log('result.response',result.response)
+    return result.response[0];
+  }
+  catch (e) {
+    console.log('Error.', e)
+  }
+}
+
+getStandings().then(data => {
+  console.log(data.league.standings[0]);
+})
